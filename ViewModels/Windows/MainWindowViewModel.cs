bb1d212950741784a1b6230550d05f62c934107d
MHalo <chinatmall@outlook.com>
2024-01-24 09:33:47 +0800

    初始化项目

---
+++ ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Collections.ObjectModel;
+using Wpf.Ui.Controls;
+
+namespace UnrealEngineXRLauncher.ViewModels.Windows
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _applicationTitle = "虚幻引擎XR直播 - V1.0.0";
+
+        [ObservableProperty]
+        private ObservableCollection<object> _menuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "商城",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
+                TargetPageType = typeof(Views.Pages.DashboardPage)
+            },
+            new NavigationViewItem()
+            {
+                Content = "直播",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Video24 },
+                TargetPageType = typeof(Views.Pages.DataPage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<object> _footerMenuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "设置",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
+                TargetPageType = typeof(Views.Pages.SettingsPage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<MenuItem> _trayMenuItems = new()
+        {
+            new MenuItem { Header = "Home", Tag = "tray_home" }
+        };
+    }
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using UnrealEngineXRLauncher.ViewModels.Windows;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+
+namespace UnrealEngineXRLauncher.Views.Windows
+{
+    public partial class MainWindow
+    {
+        public MainWindowViewModel ViewModel { get; }
+
+        public MainWindow(
+            MainWindowViewModel viewModel,
+            INavigationService navigationService,
+            IServiceProvider serviceProvider,
+            ISnackbarService snackbarService,
+            IContentDialogService contentDialogService
+        )
+        {
+
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+
+            navigationService.SetNavigationControl(NavigationView);
+            snackbarService.SetSnackbarPresenter(SnackbarPresenter);
+            contentDialogService.SetContentPresenter(RootContentDialog);
+
+            NavigationView.SetServiceProvider(serviceProvider);
+            if (NavigationView.TitleBar is not null)
+            {
+                NavigationView.TitleBar!.Margin = new Thickness(0, 0, 0, 0);
+            }
+
+        }
+    }
+}
